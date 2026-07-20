15a6dab0413d09be97b1e01d643df5eee99a93c8
SHUBH SINGH <workforshubhsingh@gmail.com>
2026-07-20 17:31:24 +0530

    Add UTM parameters to app download URL for traffic tracking.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,16 @@
-export const APP_STORE_URL = "https://support.gradright.com/redirect-to-store";
+const APP_STORE_BASE_URL = "https://support.gradright.com/redirect-to-store";
+
+export const APP_STORE_UTM = {
+  source: "bad-advice",
+  campaign: "event-2026-july",
+  medium: "web",
+} as const;
+
+export const APP_STORE_URL = `${APP_STORE_BASE_URL}?${new URLSearchParams({
+  utm_source: APP_STORE_UTM.source,
+  utm_medium: APP_STORE_UTM.medium,
+  utm_campaign: APP_STORE_UTM.campaign,
+}).toString()}`;
 
 export const SIGNUP_URL =
   "https://gradright.com/get-bad-advice/?showSignUp=1";
